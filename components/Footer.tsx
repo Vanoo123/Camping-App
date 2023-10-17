33f7effc241cea112a298ee4225cc14bfc88bd68
Ivane Tvildiani <vanotvildiani7@gmail.com>
2023-10-17 15:49:47 +0400

    fixed headings new

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -73,7 +73,7 @@ type FooterColumnProps = {
 const FooterColumn = ({ title, children }: FooterColumnProps) => {
   return (
     <div className="flex flex-col gap-5">
-      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      <h5 className="bold-18 whitespace-nowrap">{title}</h5>
       {children}
     </div>
   )
