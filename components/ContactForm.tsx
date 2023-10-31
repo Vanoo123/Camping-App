01809ea99e9be644873dd7c684453d7f7583b7ef
Ivane Tvildiani <vanotvildiani7@gmail.com>
2023-11-01 00:31:26 +0400

    added form with function

---
+++ components/ContactForm.tsx
@@ -0,0 +1,54 @@
+"use client";
+import Button from './Button' 
+import { useRef } from "react"
+import emailjs from "@emailjs/browser"
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/ReactToastify.css'
+import {useLang} from "./LangProvider"
+
+export default function ContactForm() {
+    const form: any=useRef()
+
+    const sendmail=(e: any)=> {
+        e.preventDefault()
+        emailjs.sendForm("service_ycfvw4c", "template_xq7654m",form.current,"2iTJ5kXczUqV3RFzf")
+        .then(()=>{
+            toast.success("message send")
+        },()=>{
+            toast.error("somesthing is wrong")
+        })
+        e.target.reset()
+    }
+
+    const { lang } = useLang();
+    return <>
+        <form ref={form} onSubmit={sendmail} className='py-4 flex flex-col mt-4 border-t gap-5'>
+            <div>
+                <label htmlFor="fullname">{lang['formFullName']}</label>
+                <input name='fullname' type="text" id="fullname" placeholder="John Doe"/>
+            </div>
+
+            <div>
+                <label htmlFor="email">{lang['formEmail']}</label>
+                <input name='email' type="text" id="email" placeholder="example@example.com"/>
+            </div>
+
+            <div>
+                <label htmlFor="phone">{lang['formPhone']}</label>
+                <input name='phone' type="tel" id="phone" placeholder="+1 (123) 456-7890"/>
+            </div>
+            
+            <div>
+                <label htmlFor="message">{lang['formMessage']}</label>
+                <textarea name='message' className='h-32 noresize' id="message" placeholder={lang['formMessagePlaceHolder']}></textarea>
+            </div>
+            
+            <Button variant="btn_contact_send" type='submit' title={lang['formSendButton']} alt={''} />
+        </form>
+        <ToastContainer 
+        position='top-center'
+        hideProgressBar={true}
+        theme='light'
+        autoClose={2000}/>
+    </>
+}
--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,36 +1,18 @@
 "use client";
 import React from 'react'
-import Button from './Button'
-import Image from 'next/image'
+import ContactForm from '@/components/ContactForm'
+import {useLang} from "./LangProvider"
 
 const GetApp = () => {
+  const { lang } = useLang();
   return (
-    <section className="flexCenter w-full flex-col pb-[100px]">
+    <section className="flex-center w-full flex-col pb-[100px]">
       <div className="get-app" id='contact'>
-        {/* <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
-          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Get Married now!</h2>
-          <p className="regular-16 text-gray-10">Join Us</p>
-          <div className="flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row">
-            <Button 
-              type="button"
-              title="App Store"
-              icon="/apple.svg"
-              variant="btn_white"
-              full
-            />
-            <Button 
-              type="button"
-              title="Play Store"
-              icon="/android.svg"
-              variant="btn_dark_green_outline"
-              full
-            />
-          </div>
+        <div className='p-4 max-w-2xl mx-auto'>
+          <h1 className='text-3xl font-bold'>{lang['formTitle']}</h1>
+          <p>{lang['formDescription']}</p>
+          <ContactForm />
         </div>
-
-        <div className="flex flex-1 items-center justify-end">
-          <Image src="/phones.png" alt="phones" width={550} height={870} />
-        </div> */}
       </div>
     </section>
   )
