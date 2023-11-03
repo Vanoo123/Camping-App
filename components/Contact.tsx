da91c9af7776fa434b1682fe6c2a54a50f41f80e
Ivane Tvildiani <vanotvildiani7@gmail.com>
2023-11-03 15:47:14 +0400

    mini-change

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -15,16 +15,16 @@ function Contact() {
             <Link className='phone xl:text-5xl text-3xl font-bold underline-hover-phone socials' href="tel:+995-593-22-00-38">+995-593-22-00-38</Link>
             <Link className='phone xl:text-4xl text-2xl font-bold underline-hover-phone socials' href="mailto:zdarova@gmail.com">Vanotvildiani7@gmail.om</Link>
             <div className='flex gap-10 font-bold'>
-                <Link className='underline-hover socials facebook' href="https://www.facebook.com/your_facebook_page">
+                <Link className='underline-hover-socials socials facebook' href="https://www.facebook.com/your_facebook_page">
                     Facebook
                 </Link>
-                <Link className='underline-hover socials instagram' href="https://www.instagram.com/your_instagram_page">
+                <Link className='underline-hover-socials socials instagram' href="https://www.instagram.com/your_instagram_page">
                     Instagram
                 </Link>
-                <Link className='underline-hover socials whatsapp' href="https://wa.me/995593220038">
+                <Link className='underline-hover-socials socials whatsapp' href="https://wa.me/995593220038">
                     WhatsApp
                 </Link>
-                <Link className='underline-hover socials telegram' href="https://te.me/">
+                <Link className='underline-hover-socials socials telegram' href="https://te.me/">
                     Telegram
                 </Link>
             </div>
