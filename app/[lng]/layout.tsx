d60a4c9d9bbd66a3eceb6cee1c91669357e7e58b
Ivane Tvildiani <vanotvildiani7@gmail.com>
2023-11-26 18:34:44 +0400

    added one more icon

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -30,6 +30,8 @@ const RootLayout: React.FC<RootLayoutProps> = async ({ children, params: { lng }
       <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png"/>
       <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png"/>
       <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png"/>
+      <link rel="icon" type="image/png" sizes="128x128"href="/assets/favicon-128x128.png"/>
+      <link rel="icon" type="image/png" sizes="192x192"href="/assets/icon-192x192.png"/>
       <link rel="manifest" href="/assets/manifest.json" />
       <meta name="description" content={t('metaData.description')}/>
       <meta name='google-site-verification' content='PUy3W-uUr3Rz0eMwYpPe5-8THu0lWbvXWAmYl_5wjDc'/>
